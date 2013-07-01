a3a3bb3d0010c923163134925c8f0bbb51562639
Jimmyc <jimmyc42@gmail.com>
2013-07-01 15:28:39 -0500

    annoying Terra

--- add_numb.py
+++ add_numb.py
@@ -5,6 +5,7 @@ def main():
     print("2 + 2 + 2 =",add_three(2,2,2))
     print ("2 + 3 =",add_two(2,3))
     print('hello OK')
+    print('dammit stop changing my code, Terra')
 
 if __name__ == "__main__":
     main()
